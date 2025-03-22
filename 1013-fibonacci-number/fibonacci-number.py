ffbdc819da499e9cb178a1a868954ed623bd1456
Yuval Kogan <109174220+KoganTheDev@users.noreply.github.com>
2025-03-22 19:37:34 +0200

    Time: 20 ms (50.08%) | Memory: 12.4 MB (80.83%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        opt = [0, 1]
+        
+        for i in range(2, n + 1):
+            opt.append(opt[i - 1] + opt[i - 2])    
+        
+        return opt[n]
